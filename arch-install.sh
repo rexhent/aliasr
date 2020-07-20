f8cd39e0f4dddcbdbc60ae2b7e27c3901d01f564
rexhent <67210734+rexhent@users.noreply.github.com>
2020-07-20 12:26:01 +1000

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo pacman -S flatpak git vim nano sudo networkmanager reflector --noconfirm
+sudo pacman -S flatpak git vim nano sudo networkmanager reflector
 sudo reflector --country Australia -p http --save /etc/pacman.d/mirrorlist
 sudo timedatectl set-timezone Australia/Sydney
 alias ..="cd .."
