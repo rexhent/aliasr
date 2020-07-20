ed9a89c997e370494310a036fe094960097dcfac
rexhent <67210734+rexhent@users.noreply.github.com>
2020-07-20 12:22:46 +1000

    Delete arch-install.sh

--- arch-install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sudo pacman -S flatpak git vim nano sudo networkmanager reflector --noconfirm
-sudo reflector --country Australia -p http --save /etc/pacman.d/mirrorlist
-sudo timedatectl set-timezone Australia/Sydney
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-sudo pacman -S steam lutris wine --noconfirm
